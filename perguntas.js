7400d7f03bac1914fafd3199c22f7afce58f8ae4
aboboradasilva <00001111561473sp@al.educacao.sp.gov.br>
2024-11-08 12:45:24 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,102 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    `idioma`,
+    `idioma do Brasil`,
+    `portugues`
+)
+
+
+criaCartao(
+    `filme`,
+    `gênero`,
+    `Ação`
+)
+
+
+criaCartao(
+    `jogo`,
+    `jogo de tabuleiro`,
+    `Xadrez`
+)
+
+
+criaCartao(
+    `filme`,
+    `gênero preferido`,
+    `Ação`
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Tnoevry Grvkrven, niryvab*/
